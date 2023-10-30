1c7b591eb148eec00ffc1d5775480c005795c3f9
ocoskun2023 <omer.cod@gmail.com>
2023-10-30 14:34:45 +0100

    base ,login and register,home update

--- src/Controller/MonProfilController.php
+++ src/Controller/MonProfilController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LoginController extends AbstractController
+class MonProfilController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
+    #[Route('/mon_profil', name: 'app_mon_profil')]
     public function index(): Response
     {
-        return $this->render('login/login.html.twig', [
-            'controller_name' => 'LoginController',
+        return $this->render('mon_profil/mon_profil.html.twig', [
+            'controller_name' => 'MonProfilController',
         ]);
     }
 }
