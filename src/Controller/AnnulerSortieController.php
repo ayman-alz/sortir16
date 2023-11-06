e649571f45c034edafbfc018483ea91716f8683b
ocoskun2023 <omer.cod@gmail.com>
2023-11-06 15:35:25 +0100

    edit update

--- src/Controller/AnnulerSortieController.php
+++ src/Controller/AnnulerSortieController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnnulerSortieController extends AbstractController
 {
-    #[Route('/annuler_sortie', name: 'app_annuler_sortie')]
-    public function index(): Response
+    #[Route('/annuler_sortie/{id}', name: 'app_annuler_sortie')]
+    public function delete(SortieRepository $sortieRepository,$id, EntityManagerInterface $em, ): Response
     {
-        return $this->render('annuler_sortie/annuler_sortie.html.twig', [
-            'controller_name' => 'AnnulerSortieController',
-        ]);
+        $sortie= $sortieRepository->find($id);
+        $em->remove($sortie);
+        $em->flush();
+        $this->addFlash('success', 'La sortie a été supprimée');
+
+
+        return $this->redirectToRoute('app_home');
     }
 }
