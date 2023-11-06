28f5755509dd9a3c7c05f47b269ca6721cbe1602
ocoskun2023 <omer.cod@gmail.com>
2023-11-06 16:56:44 +0100

    edit update

--- src/Controller/ModifierSortieController.php
+++ src/Controller/ModifierSortieController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieFormType;
+use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ModifierSortieController extends AbstractController
 {
-    #[Route('/modifier_sortie', name: 'app_modifier_sortie')]
-    public function index(): Response
+    #[Route('/modifier_sortie/{id}', name: 'app_modifier_sortie',requirements: ['id' => '\d+'], methods: ['POST','GET'])]
+    public function index(EntityManagerInterface $em,Sortie $sortie,  VilleRepository  $villeRepository,Request $request): Response
     {
+        $villes = $villeRepository->findAll();
+
+        $form_sortie = $this->createForm(SortieFormType::class,$sortie);
+        $form_sortie->handleRequest($request);
+
         return $this->render('modifier_sortie/modifier_sortie.html.twig', [
-            'controller_name' => 'ModifierSortieController',
+            'sortie'=>$sortie,
+            'form_sortie'=>$form_sortie,
+            'cities' => $villes,
+
         ]);
     }
 }
--- src/Form/SortieFormType.php
+++ src/Form/SortieFormType.php
@@ -2,19 +2,15 @@
 
 namespace App\Form;
 
-use App\Entity\Etat;
-use App\Entity\Lieu;
 use App\Entity\Sortie;
-use App\Entity\Ville;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Entity\Campus;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
 class SortieFormType extends AbstractType
@@ -25,12 +21,14 @@ class SortieFormType extends AbstractType
             ->add('nom')
             ->add('dateHeureDebut', DateType::class,
                 [
-                    'input' => 'datetime_immutable'
+                    'widget' => 'single_text',
+                    'html5' => true,
                 ])
             ->add('duree')
             ->add('dateLimiteInscription', DateType::class,
                 [
-                    'input' => 'datetime_immutable'
+                    'widget' => 'single_text',
+                    'html5' => true,
                 ]
             )
             ->add('nbInsciptionMax')
