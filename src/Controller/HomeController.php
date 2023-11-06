2e6cb51c5395b63e3a2c851238995224c6d30b87
ocoskun2023 <omer.cod@gmail.com>
2023-11-06 13:32:51 +0100

    edit update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
+use App\Entity\Sortie;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
+use function Symfony\Component\Clock\now;
 
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home',methods: ['GET', 'POST'])]
-    public function index(SortieRepository $sortieRepository,Request $request, PaginatorInterface $paginator): Response
+    public function index(SortieRepository $sortieRepository,Request $request, PaginatorInterface $paginator,EntityManagerInterface $em): Response
     {
+        $this->updateEtat($sortieRepository,$em);
         $sorties = $sortieRepository->findAll();
 
         $page = $request->query->getInt('page', 1);
@@ -29,5 +35,54 @@ class HomeController extends AbstractController
             'sorties' => $sorties,
             'pagination' => $pagination
         ]);
+    }
+
+    #[Route('/publier/{id}', name: 'app_home-publier',methods: ['GET', 'POST'])]
+    public function publierSortie(SortieRepository $sortieRepository,EntityManagerInterface $em,$id): Response
+    {
+        $sortie = $sortieRepository->find($id);
+        $sortie->getEtat()->setLibelle(Etat::PUBLIER);
+
+        $em->persist($sortie);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+    #[Route('/sinscrir/{id}', name: 'app_home-inscrir',methods: ['GET', 'POST'])]
+    public function sinscrirSortir(SortieRepository $sortieRepository,EntityManagerInterface $em,$id): Response
+    {
+        $sortie = $sortieRepository->find($id);
+        $sortie->getEtat()->setLibelle(Etat::PUBLIER);
+
+        $em->persist($sortie);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+    #[Route('/sdesister/{id}', name: 'app_home-desister',methods: ['GET', 'POST'])]
+    public function sdesisterSortie(SortieRepository $sortieRepository,EntityManagerInterface $em,$id): Response
+    {
+        $sortie = $sortieRepository->find($id);
+        $sortie->getEtat()->setLibelle(Etat::PUBLIER);
+
+        $em->persist($sortie);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    public function updateEtat(SortieRepository $sortieRepository,EntityManagerInterface $em ){
+
+        $sorties = $sortieRepository->findAll();
+        foreach ($sorties as $sortie){
+            if($sortie->getEtat()->getLibelle() === Etat::PUBLIER){
+                if ($sortie->getDateLimiteInscription() < new \DateTime()) {
+                    $sortie->getEtat()->setLibelle(Etat::TERMINE); // Çıkışın durumunu ayarla
+                    $em->persist($sortie);
+                    $em->flush();
+                }
+            }
+
+        }
     }
 }
